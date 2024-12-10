08603197b2999bff21c15cbea728f0ecfc068101
Elmersperry <rodionstudent@mail.ru>
2024-12-10 21:10:07 +0300

    hyhyhhhhhhhh

--- scripts/currency.py
+++ scripts/currency.py
@@ -18,6 +18,8 @@
 # URL = "https://www.alta.ru/currency/"
 # html = get_html(URL)
 # parse_html(html)
+import json
+from textwrap import indent
 
 import requests
 from bs4 import BeautifulSoup as BS
@@ -28,14 +30,14 @@ def get_HTML(url: str) -> str|None:
         status = response.status_code
         if status != 200 and str(status)[0] != 3:  # 404,503,301
             # print(f'Ошибка запроса. Код ответа - {status}')
-            return None
             print(f"Код ответа - {status}")
+            return None
         html = response.text
     except Exception as error:
         print('Нет ответа от сервера')
         print(error)
         return None
-def parse_HTML(html: str):
+def parse_html(html: str):
     soup: BS = BS(html, "html.parser")
     current_date = (soup.find("h2", class_="h2 blue").text.split("\n")[-1].strip()[:10])
     table = soup.find("div", class_="module-tableSort")
@@ -51,8 +53,21 @@ def parse_HTML(html: str):
         print(f'{code} {name} {value}')
         pass
 
+def write_data_to_json(data: dict) -> None:
+    with open("courses.json", "w", encoding="utf-8") as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
+
+def get_data_from_json(file_path: str) -> str:
+    with open(file_path, "r", encoding="utf-8") as file:
+        data = json.load(file)
+    return data
+
 url = "https://www.alta.ru/currency/"
 html = get_HTML(url)
 if html:
-    parse_HTML(html)
+    courses = parse_html(html)
+    write_data_to_json(data=courses)
+
+
+
 # почитать про статус-код (400 - ошибки на стороне клиента, 500 - ошибки на стороне сервера)
