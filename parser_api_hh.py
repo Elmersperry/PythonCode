5f446ff4b4c6ba668607f210ea372ead23394034
Elmersperry <rodionstudent@mail.ru>
2024-12-24 22:02:09 +0300

    jjj

---
+++ parser_api_hh.py
@@ -0,0 +1,47 @@
+from cgi import parse
+
+import requests
+
+def get_vacancies_pages(url: str):
+    area = 2
+    per_page = 100
+    text = "python"
+    params = {
+        "area": area,
+        "per_page": per_page,
+        "text": text
+    }
+    resp = requests.get(url, params=params)
+    data = resp.json()
+    return data["pages"], data["found"]
+
+def get_data_page(url: str, page: int):
+    params = {
+        "area": 2,
+        "per_page": 100,
+        "page": page,
+        "text": "python"
+    }
+    resp = requests.get(url, params=params)
+    data = resp.json()
+    return data
+
+def parse_page(data: dict):
+    vacancies = {}
+    items = data["items"]
+    for i in items:
+        vacancies[i["id"]] = items[i]["name"]
+    return  vacancies
+
+def main():
+    vacancies = []
+    url_api = "https://api.hh.ru/vacancies"
+    pages, found = get_vacancies_pages(url=url_api)
+    print(f"Найдено {found} вакансий")
+    for i in range(1, pages+1):
+        data_page = get_data_page(url=url_api, page=i)
+        vacancies.append(parse_page(data_page))
+    print(*vacancies, sep="\n")
+
+url_api = "https://api.hh.ru/vacancies"
+pages, found = get_vacancies_pages(url=url_api)
